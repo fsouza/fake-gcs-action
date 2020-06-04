b2d01df12321b9587cd06057fd843b465c93fb93
francisco souza <fsouza@users.noreply.github.com>
2020-06-04 11:08:33 -0400

    entrypoint: split docker_args from fake-gcs-server args

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,10 +3,15 @@
 args=(
 	-backend "${INPUT_BACKEND}"
 )
+docker_args=(
+	--detach
+	--publish 8443:8443
+)
 
 if [ -n "${INPUT_DATA}" ]; then
 	INPUT_DATA=$(realpath "${INPUT_DATA}")
-	args+=(-data "${INPUT_DATA}" -v "${INPUT_DATA}:${INPUT_DATA}")
+	args+=(-data "${INPUT_DATA}" )
+	docker_args+=(--volume "${INPUT_DATA}:${INPUT_DATA}")
 fi
 
 if [ -n "${INPUT_EXTERNAL_URL}" ]; then
@@ -17,4 +22,4 @@ if [ -n "${INPUT_PUBLIC_HOST}" ]; then
 	args+=(-public-host "${INPUT_PUBLIC_HOST}")
 fi
 
-exec docker run -d --publish 4443:4443 "fsouza/fake-gcs-version:${INPUT_VERSION}" "${args[@]}"
+exec docker run "${docker_args[@]}" "fsouza/fake-gcs-version:${INPUT_VERSION}" "${args[@]}"
