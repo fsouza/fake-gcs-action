ba620502adc1468250a4784b0e9405e54ffeb4b9
francisco souza <108725+fsouza@users.noreply.github.com>
2020-06-13 09:18:38 -0400

    Fix entrypoint and add a test (#2)
    * Add a basic test
    
    * Fix entrypoint
    
    * entrypoint: split docker_args from fake-gcs-server args
    
    * github/workflows: fix step name
    
    * entrypoint: fix image name
    
    * Make curl verbose in test
    
    * Fix curl command in test
    
    * Add shellcheck to CI too
    
    * Rename GH action file
    
    * reame: fix port
    
    * github/workflows: fix port number
    
    * entrypoint: fix port number and add logic for waiting for server
    
    * entrypoint: fix timeout call
    
    Gotta love some alpine.
    
    * debug
    
    * debug
    
    * debuggggggggggggggg
    
    * docker info
    
    * Fix variables
    
    * entrypoint: use docker exec to wait for server
    
    The action container is using a mounted socket to communicate with the
    docker server, therefore the port is not available in the container
    running the action.
    
    * entrypoint: fix variable expansion
    
    * github/workflows: remove --verbose from curl call
    
    * entrypoint: cd to /github/workspace before running
    
    This way the data can be mounted properly. Also add validation against
    the provided input data.
    
    * Add some debugging information, gated by a new config param
    
    * entrypoint: remove debugging flag
    
    * entrypoint: use RUNNER_WORKSPACE instead of /github/workspace
    
    My bad my bad
    
    * [DEBUG] Also log GITHUB_WORKSPACE
    
    * kill me
    
    * entrypoint: fix mounting
    
    This is a weird hack x)
    
    Co-authored-by: francisco souza <fsouza@users.noreply.github.com>

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,9 +4,28 @@ args=(
 	-backend "${INPUT_BACKEND}"
 )
 
+docker_args=(
+	--detach
+	--publish 4443:4443
+)
+
+INPUT_EXTERNAL_URL=$(printenv INPUT_EXTERNAL-URL)
+INPUT_PUBLIC_HOST=$(printenv INPUT_PUBLIC-HOST)
+
+docker_image=fsouza/fake-gcs-server:${INPUT_VERSION}
+
 if [ -n "${INPUT_DATA}" ]; then
-	INPUT_DATA=$(realpath "${INPUT_DATA}")
-	args+=(-data "${INPUT_DATA}" -v "${INPUT_DATA}:${INPUT_DATA}")
+	if ! [ -d "${INPUT_DATA}" ]; then
+		echo "ERROR: input data should be a directory. Make sure it exists and is specified as a relative path" >&2
+		exit 2
+	fi
+
+	if [ -n "${INPUT_DEBUG}" ]; then
+		echo "RUNNER_WORKSPACE=${RUNNER_WORKSPACE}"
+	fi
+	INPUT_DATA=${RUNNER_WORKSPACE}/fake-gcs-action/${INPUT_DATA}
+	args+=(-data "${INPUT_DATA}" )
+	docker_args+=(--volume "${INPUT_DATA}:${INPUT_DATA}")
 fi
 
 if [ -n "${INPUT_EXTERNAL_URL}" ]; then
@@ -17,4 +36,20 @@ if [ -n "${INPUT_PUBLIC_HOST}" ]; then
 	args+=(-public-host "${INPUT_PUBLIC_HOST}")
 fi
 
-exec docker run -d --port 4443:4443 "fsouza/fake-gcs-version:${INPUT_VERSION}" "${args[@]}"
+docker pull "${docker_image}"
+container_id=$(docker run "${docker_args[@]}" "${docker_image}" "${args[@]}")
+
+timeout=10
+echo "waiting up to ${timeout}s for server to come up"
+if ! timeout 10 bash -c 'while ! docker exec '"${container_id}"' wget -qO /dev/null -T 1 --no-check-certificate https://0.0.0.0:4443/storage/v1/b; do echo "server not available yet, sleeping..."; sleep 1; done'; then
+	echo "Failed to connect to the server after ${timeout}s" >&2
+	echo "Logs from docker: " >&2
+	docker logs "${container_id}"
+	exit 1
+fi
+
+echo "Server started successfully!"
+if [ -n "${INPUT_DEBUG}" ]; then
+	echo "Logs from container: "
+	docker logs "${container_id}"
+fi
